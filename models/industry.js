5dc067df6db14108dcd27843ba274d6e7d61a8a0
Justin <hello@justintlai.com>
2017-11-13 14:27:18 +0000

    updated Model structure

--- models/industry.js
+++ models/industry.js
@@ -1,19 +1,19 @@
-// module.exports = (sequelize, DataTypes) => {
-//   return sequelize.define(
-//     'Industry',
-//     {
-//       code: {
-//         type: DataTypes.INTEGER,
-//         primaryKey: true
-//       },
-//       description: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//       }
-//     },
-//     {
-//       timestamps: false,
-//       freezeTableName: true
-//     }
-//   );
-// };
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "Industry",
+    {
+      code: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      timestamps: false,
+      freezeTableName: true
+    }
+  );
+};
