5f3ddac608265178e4eacf9e2ee09b43f72a972a
Justin <hello@justintlai.com>
2017-11-14 11:07:03 +0000

    seed files added

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 /**
  * Get port from environment and store in Express.
  */
-var port = normalizePort(process.env.PORT || "5000");
+var port = normalizePort(process.env.PORT || "5001");
 app.set("port", port);
 /**
  * Create HTTP server.
@@ -38,6 +38,8 @@ models.sequelize
   })
   .then(() => {
     loadFile("./industry.json", models);
+    loadFile("./userPrefs.json", models);
+    loadFile("./users.json", models);
     // .then(() => console.log("success"))
     // .catch(err) => console.log(`loading ${err}`));
   });
